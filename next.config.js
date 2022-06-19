3c2960725010da3366e77a7b7a584ee56300e00b
charliedeus <charliedeus@gmail.com>
2022-06-19 13:22:05 -0300

    Adding moduleResolution node on eslintrc.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost:3000',
+      'placeimg.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
