47bbabba6e75cf5f9d4b004427773d7b412aa145
charliedeus <charliedeus@gmail.com>
2022-06-19 15:51:14 -0300

    Creating a Header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import Layout from '../components/Layout/MyLayout';
+import Layout from '../components/Layout/Layout';
 
 export default function Home() {
   return <h1>Hello World!</h1>;
